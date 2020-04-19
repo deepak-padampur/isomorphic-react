fc63ae8de5b157de5df83ae36c787945c2c78a19
deepak-padampur <deepaksuna4290@gmail.com>
2020-04-19 23:06:57 +0530

    integrated webpack with babel

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import yields from 'express-yields';//For using the generators
 import fs from 'fs-extra';
+import webpack from 'webpack';
 //instanciate express
 const app = express();
 const port = process.env.PORT || 3000;
+if (process.env.NODE_ENV === 'development') {
+  const config = require('../webpack.config.dev.babel').default;
+  const compiler = webpack(config);
+
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true
+  }));
+  app.use(require('webpack-hot-middleware')(compiler))
+
+}
+
 app.get(['/'], function* (req, res) {
-  let index = yield fs.readFile(`../public/index.html`, 'utf-8');
+  let index = yield fs.readFile(`./public/index.html`, 'utf-8');
   res.send(index);
 })
 
