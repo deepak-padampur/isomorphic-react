fbf362608df36b063483ad85cff9763891bdaa18
deepak-padampur <deepaksuna4290@gmail.com>
2020-04-19 23:43:59 +0530

    api route added to get questions

--- server/index.js
+++ server/index.js
@@ -2,9 +2,36 @@ import express from 'express';
 import yields from 'express-yields';//For using the generators
 import fs from 'fs-extra';
 import webpack from 'webpack';
+import { argv } from 'optimist';
+import { get } from 'request-promise';
+import { questions, question } from '../data/api-url';
+
+import { delay } from 'redux-saga';
 //instanciate express
 const app = express();
 const port = process.env.PORT || 3000;
+
+const useLiveData = argv.useLiveData === 'true';
+
+function* getQuestions() {
+  let data;
+  //if we are using the live data
+  if (useLiveData) {
+    data = yield get(questions, { gzip: true })
+
+  } else {
+    data = yield fs.readFile(`./data/questions.json`, "utf-8");
+  }
+
+  return JSON.parse(data);
+}
+
+//make an  api route to handle
+app.get(['/api/questions'], function* (req, res) {
+  const data = yield getQuestions();
+  yield delay(150);
+  res.json(data);
+})
 if (process.env.NODE_ENV === 'development') {
   const config = require('../webpack.config.dev.babel').default;
   const compiler = webpack(config);
